47e96ee7161bd96d9b153d053cd0bc8fbbe44c13
Nourhan Sherif <Norhansherif032@gmail.com>
2026-01-11 15:21:53 +0200

    Connected to Database successfully

--- practice projects/Shopping cart/app.js
+++ practice projects/Shopping cart/app.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 // use express
 const port = 3000;
 const app = express();
+
+// connect to db 
+mongoose.connect(process.env.database)
+    .then(() => {
+        console.log(`connected to db`);
+    });
 
 // set views 
 app.set('views', path.join(__dirname, 'views'));
