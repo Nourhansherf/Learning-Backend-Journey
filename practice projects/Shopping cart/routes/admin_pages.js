ac60866beb1c328fd203e2e24230d9db58442488
Nourhan Sherif <Norhansherif032@gmail.com>
2026-01-11 20:25:12 +0200

    Added some functionality to administration header and routes.

--- practice projects/Shopping cart/routes/admin_pages.js
+++ practice projects/Shopping cart/routes/admin_pages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 /**
  * GET pages index
@@ -24,6 +25,39 @@ router.get('/add-page', (req, res) => {
         content: content
     })
 });
+
+/**
+ * POST add page
+ */
+router.post('/add-page',
+    // validation middleware
+    [
+        body('title').notEmpty().withMessage('Title must have a value'),
+        body('content').notEmpty().withMessage('Content must have a value')
+    ],
+    (req, res) => {
+
+        let title = req.body.title;
+        let slug = (req.body.slug || '').replace(/\s+/g, '-').toLowerCase();
+        let content = req.body.content;
+
+        if (slug == "") slug = title.replace(/\s+/g, '-').toLowerCase();
+
+        const errorsResult = validationResult(req);
+
+        if (!errorsResult.isEmpty()) {
+            res.render('admin/add_page', {
+                errors: errorsResult.array(),
+                title: title,
+                slug: slug,
+                content: content
+            });
+        } else {
+            console.log('success');
+            // TODO: save page to DB
+        }
+
+    });
 
 // import router
 module.exports = router;
