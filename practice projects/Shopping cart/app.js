eaf89609022731a7d27b26e40cddd36903c891b3
Nourhan Sherif <Norhansherif032@gmail.com>
2026-01-11 16:13:47 +0200

    added some dependencies like bodyparser express-validator express-session express-messages

--- practice projects/Shopping cart/app.js
+++ practice projects/Shopping cart/app.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+var session = require('express-session');
+const expressvalidator = require('express-validator');
+// body-parser express-session express-validator express message
 
 // use express
 const port = 3000;
@@ -20,6 +23,43 @@ app.set('view engine', 'ejs');
 
 // use public 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// bodyparser middleware
+app.use(bodyParser.urlencoded({ urlencoded: false }));
+app.use(bodyParser.json());
+
+// express session conf
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+// express validation middle ware
+app.use(expressvalidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+        
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+// express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
 
 // Router configuration
 const pages = require('./routes/pages');
