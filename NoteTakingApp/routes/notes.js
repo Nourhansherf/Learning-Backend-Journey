7847c7552b7c2f6ab659e478c637fdc9e1e75ff0
Nourhan Sherif <Norhansherif032@gmail.com>
2026-01-13 22:46:34 +0200

    Added the functionality of adding notes

--- NoteTakingApp/routes/notes.js
+++ NoteTakingApp/routes/notes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Note = require('../models/notes');
 
 const isAuth = (req, res, next) => {
     if (req.session.isAuth) {
@@ -10,13 +11,36 @@ const isAuth = (req, res, next) => {
 }
 
 // notes
-router.get('/', isAuth , (req, res) => {
-    res.render('notes', { title: 'Your Notes' });
+router.get('/', isAuth, async (req, res) => {
+    try {
+        const notes = await Note.find().lean();
+        res.render('notes', { title: 'Your Notes', data: notes });
+    } catch (err) {
+        console.error(err);
+        res.render('error', { message: 'Unable to load notes', error: err });
+    }
 });
 
 //add note
 router.get('/add-note', isAuth, (req, res) => {
     res.render('add-note', { title: 'Add Note' });
 });
+
+// post create note
+router.post('/add-note', isAuth, async (req, res) => {
+    try {
+        const newNote = new Note({
+            title: req.body.title,
+            body: req.body.body,
+        });
+
+        await newNote.save();
+        res.redirect('/notes');
+    } catch (err) {
+        console.error(err);
+        res.render('error', { message: 'Unable to add note', error: err });
+    }
+});
+
 
 module.exports = router;
