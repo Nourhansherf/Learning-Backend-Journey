baedcadd79b3d0b4b461774176878dcc5fc88a87
Nourhan Sherif <Norhansherif032@gmail.com>
2026-01-13 21:28:08 +0200

    Added the login frontend

--- NoteTakingApp/app.js
+++ NoteTakingApp/app.js
@@ -9,9 +9,10 @@ const connectDB = require('./config/db');
 const port = process.env.PORT || 3000;
 
 //routes
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const registerRouter = require('./routes/register');
+// const indexRouter = require('./routes/index');
+// // const usersRouter = require('./routes/users');
+// const registerRouter = require('./routes/register');
+// const loginRouter = require('./routes/login');
 
 const app = express();
 //connect DB
@@ -29,9 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Router configurations
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/register', registerRouter);
+app.use('/', require('./routes/index'));
+// app.use('/users', usersRouter);
+app.use('/register', require('./routes/register'));
+app.use('/login', require('./routes/login'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
